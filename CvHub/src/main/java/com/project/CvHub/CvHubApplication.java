d2f67889fb414f448bf4e15ffc42c78dd0507adf
Ha\meipn962003 <21110766@student.hcmute.edu.vn>
2025-02-16 12:46:33 +0700

    init prj

---
+++ CvHub/src/main/java/com/project/CvHub/CvHubApplication.java
@@ -0,0 +1,13 @@
+package com.project.CvHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvHubApplication.class, args);
+	}
+
+}
---
+++ CvHub/src/test/java/com/project/CvHub/CvHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.CvHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
